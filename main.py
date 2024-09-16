525bec28abfad4822a0720a541aa8272a70062d4
DOOMonichan <DOOM-on@mail.ru>
2024-09-16 18:31:38 +0500

    v0.13

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import asyncio
 import logging
-import aiosqlite
 from test import *
-import pandas as pd
 from aiogram import F
 from aiogram import types
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+logging.basicConfig(level=logging.INFO)
 API_TOKEN = "7404804929:AAFuXD8pshvqK8JPKFRQY1yiXXDCSGWw3lQ"
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=API_TOKEN) 
 dp = Dispatcher()
 
 @dp.message(Command("start"))
@@ -24,101 +23,27 @@ async def cmd_test(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(text="В меню"))
     await message.answer(f"Давайте начнем тест!", reply_markup=builder.as_markup(resize_keyboard=True))
-    await new_test(message)
+    user_id = message.from_user.id
+    await update_test_index_points(user_id, 0, 0)
+    await get_question(message, user_id)
 
 @dp.message(F.text=="В меню")
 async def cmd_menu(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(text="Тест"))
+    builder.add(types.KeyboardButton(text="Статистика"))
     await message.answer("Нажмите 'Тест' для проверки своих знаний.", reply_markup=builder.as_markup(resize_keyboard=True))
 
-"""#file test.py    
-DB_NAME = 'test_bot.db'
-question_data = pd.read_csv("question_data.csv")
-question_data['options'] = question_data['options'].apply(lambda x: x.split(","))
-
-def generate_options_keyboard(answer_options, right_answer):
-    builder = InlineKeyboardBuilder()
-
-    for option in answer_options:
-        builder.add(types.InlineKeyboardButton(
-            text=option,
-            callback_data= "right_" + option if option == right_answer else "wrong_" + option)
-        )
-
-    builder.adjust(2)
-    return builder.as_markup()
-
-async def check_last_question(callback: types.CallbackQuery):
-    current_question_index = await get_test_index(callback.from_user.id)
-    current_question_index += 1
-    await update_test_index(callback.from_user.id, current_question_index)
-    if current_question_index < len(question_data):
-        await get_question(callback.message, callback.from_user.id)
-    else:
-        await callback.message.answer("Это был последний вопрос. Тест завершен!")
-
-@dp.callback_query(F.data.startswith("right_"))
-async def right_answer(callback: types.CallbackQuery):
-    await callback.bot.edit_message_reply_markup(
-        chat_id=callback.from_user.id,
-        message_id=callback.message.message_id,
-        reply_markup=None
-    )
-
-    await callback.message.answer(f"Ваш вариант отвера: {callback.data[6:]}, верен!")
-    await check_last_question(callback)
-
-@dp.callback_query(F.data.startswith("wrong_"))
-async def wrong_answer(callback: types.CallbackQuery):
-    await callback.bot.edit_message_reply_markup(
-        chat_id=callback.from_user.id,
-        message_id=callback.message.message_id,
-        reply_markup=None
-    )
-
-    current_question_index = await get_test_index(callback.from_user.id)
-    correct_option = question_data['correct_option'][current_question_index]
-    await callback.message.answer(f"Ваш вариант отвера: {callback.data[6:]}, не верен!")
-    await callback.message.answer(f"Правильный ответ: {question_data['options'][current_question_index][correct_option]}")
-    await check_last_question(callback)
-
-async def get_question(message, user_id):
-    current_question_index = await get_test_index(user_id)
-    correct_index = question_data['correct_option'][current_question_index]
-    opts = question_data['options'][current_question_index]
-    kb = generate_options_keyboard(opts, opts[correct_index])
-    await message.answer(f"{question_data['question'][current_question_index]}", reply_markup=kb)
-
-async def new_test(message):
-    user_id = message.from_user.id
-    current_question_index = 0
-    await update_test_index(user_id, current_question_index)
-    await get_question(message, user_id)
-
-async def get_test_index(user_id):
-     async with aiosqlite.connect(DB_NAME) as db:
-        async with db.execute('SELECT question_index FROM test_state WHERE user_id = (?)', (user_id, )) as cursor:
-            results = await cursor.fetchone()
-            if results is not None:
-                return results[0]
-            else:
-                return 0
-            
-async def update_test_index(user_id, index):
-    async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute('INSERT OR REPLACE INTO test_state (user_id, question_index) VALUES (?, ?)', (user_id, index))
-        await db.commit()
-
-async def create_table():
-    async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute('''CREATE TABLE IF NOT EXISTS test_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
-        await db.commit()
-#file test.py"""
+@dp.message(F.text=="Статистика")
+async def cmd_test(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.add(types.KeyboardButton(text="Моя статистика"))
+    builder.add(types.KeyboardButton(text="В меню"))
+    await message.answer(f"Статистка!", reply_markup=builder.as_markup(resize_keyboard=True))
 
 async def main():
-    await dp.start_polling(bot)
     await create_table()
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main()) 
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+import sys
+import pathlib
 import aiosqlite
 import pandas as pd
 from aiogram import F
-from aiogram import Dispatcher, types
+from aiogram import  Dispatcher, types
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-DB_NAME = 'test_bot.db'
+
+script_dir = pathlib.Path(sys.argv[0]).parent
+DB_NAME = script_dir / 'test_bot.db'
 question_data = pd.read_csv("question_data.csv")
 question_data['options'] = question_data['options'].apply(lambda x: x.split(","))
 dp = Dispatcher()
@@ -23,11 +27,13 @@ def generate_options_keyboard(answer_options, right_answer):
 async def check_last_question(callback: types.CallbackQuery):
     current_question_index = await get_test_index(callback.from_user.id)
     current_question_index += 1
-    await update_test_index(callback.from_user.id, current_question_index)
+    current_question_points = await get_test_points(callback.from_user.id)
+    current_question_points += int(callback.data[-1])
+    await update_test_index_points(callback.from_user.id, current_question_index, current_question_points)
     if current_question_index < len(question_data):
         await get_question(callback.message, callback.from_user.id)
     else:
-        await callback.message.answer("Это был последний вопрос. Тест завершен!")
+        await callback.message.answer(f"Это был последний вопрос. Тест завершен! Ваш результат - {current_question_points}.")
 
 @dp.callback_query(F.data.startswith("right_"))
 async def right_answer(callback: types.CallbackQuery):
@@ -36,7 +42,7 @@ async def right_answer(callback: types.CallbackQuery):
         message_id=callback.message.message_id,
         reply_markup=None
     )
-
+    current_question_index = await get_test_index(callback.from_user.id)
     await callback.message.answer(f"Ваш вариант отвера: {callback.data[6:]}, верен!")
     await check_last_question(callback)
 
@@ -61,12 +67,6 @@ async def get_question(message, user_id):
     kb = generate_options_keyboard(opts, opts[correct_index])
     await message.answer(f"{question_data['question'][current_question_index]}", reply_markup=kb)
 
-async def new_test(message):
-    user_id = message.from_user.id
-    current_question_index = 0
-    await update_test_index(user_id, current_question_index)
-    await get_question(message, user_id)
-
 async def get_test_index(user_id):
      async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute('SELECT question_index FROM test_state WHERE user_id = (?)', (user_id, )) as cursor:
@@ -75,13 +75,22 @@ async def get_test_index(user_id):
                 return results[0]
             else:
                 return 0
-            
-async def update_test_index(user_id, index):
+
+async def get_test_points(user_id):
+     async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute('SELECT points FROM test_state WHERE user_id = (?)', (user_id, )) as cursor:
+            results = await cursor.fetchone()
+            if results is not None:
+                return results[0]
+            else:
+                return 0
+
+async def update_test_index_points(user_id, index, points):
     async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute('INSERT OR REPLACE INTO test_state (user_id, question_index) VALUES (?, ?)', (user_id, index))
+        await db.execute('INSERT OR REPLACE INTO test_state (user_id, question_index, points) VALUES (?, ?, ?)', (user_id, index, points))
         await db.commit()
 
 async def create_table():
     async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute('''CREATE TABLE IF NOT EXISTS test_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
+        await db.execute('''CREATE TABLE IF NOT EXISTS test_state (user_id INTEGER PRIMARY KEY, question_index INTEGER, points INTEGER)''')
         await db.commit()
